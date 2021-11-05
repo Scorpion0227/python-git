3952299728717b4182647546ddad078bf0ae9031
Burkhon Ismoilov <undead2702@gmail.com>
2021-11-05 17:06:47 +0500

    My first commit

---
+++ is.py
@@ -0,0 +1,28 @@
+# a = 'Elbek'
+# b = 'Elbek'
+# print(id(a), id(b))
+
+
+# a = [1,2,3]
+# b = [1,2,3]
+# print(id(a), id(b))
+# print(a is b)
+
+
+# str = 'Burxon'
+# print(id(str))
+# # str [0] = 'A'
+# str = str.upper()
+# print(id(str))
+
+
+# a = [1,2,3]
+# a[0] = 20
+# print(a)
+
+
+# a = 'Burxon'
+# b = 'Burxon'
+# print(a == b, a is b, id(a), id(b))
+
+
