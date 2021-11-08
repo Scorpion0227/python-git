2b9c8b0cedee1777656eeb0623893cfd6577715b
Burkhon Ismoilov <undead2702@gmail.com>
2021-11-08 17:19:59 +0500

    My second commit

--- index.py
+++ index.py
@@ -1,2 +1,31 @@
-for item in [1, 2, 3]:
-    print(item == 2)
+# for item in [1, 2, 3]:
+#     print(item == 2)
+
+
+# word = str(input("Enter palindrom word: "))
+# x = len(word)
+# i = 0
+# x = x - 1
+# palindrom = 0
+# while x - i >= i:
+#     if word[x - i] == word[i]:
+#         i += 1
+#     else:
+#         palindrom = 1
+#         break
+# if palindrom == 1:
+#     print("yes")
+# else:
+#     print("no")
+
+
+
+# text = input('Enter text: ')
+
+# def is_palindrome(org):
+#     org = org.lower()
+#     return org[::-1] == org
+
+# print(is_palindrome(text))
+
+
